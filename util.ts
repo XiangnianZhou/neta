788d52f01cdaaf30d6a4945569a5e41173a7d736
XiangnianZhou <47637763+XiangnianZhou@users.noreply.github.com>
2020-05-20 11:47:38 +0800

    feat: util script

---
+++ util.ts
@@ -0,0 +1,22 @@
+const SECOND = 1000;
+const MINUTE = 60 * SECOND;
+const HOUR = 60 * MINUTE;
+
+export function $parseTime(timeStr: string = ""): number {
+    let match = timeStr.match(/(\d+):(\d+):(\d+),(\d+)/);
+    match = match || Array(5).fill(0);
+    const [h, m, s, ms] = match.slice(1).map((i) => +i);
+    return h * HOUR + m * MINUTE + s * SECOND + ms;
+}
+
+
+export function $merge(keyArray: string[] = [], valueArray: any[] = []): object {
+    const length = Math.min(keyArray.length, valueArray.length);
+    let obj = Object.create(null);
+    for(let i = 0; i < length; i++) {
+        const key = keyArray[i];
+        const value = valueArray[i];
+        obj[key] = value;
+    }
+    return obj;
+}
