dc0220be26313b740f48e9f57732b7f78446bfaf
XiangnianZhou <47637763+XiangnianZhou@users.noreply.github.com>
2020-05-25 21:28:04 +0800

    refactor: assign

--- util.ts
+++ util.ts
@@ -5,7 +5,7 @@ const HOUR = 60 * MINUTE;
 export function $parseTime(timeStr: string = ""): number {
     let match = timeStr.match(/(\d+):(\d+):(\d+),(\d+)/);
     match = match || Array(5).fill(0);
-    const [h, m, s, ms] = match.slice(1).map((i) => +i);
+    const [,h, m, s, ms] = match.map((i) => +i);
     return h * HOUR + m * MINUTE + s * SECOND + ms;
 }
 
